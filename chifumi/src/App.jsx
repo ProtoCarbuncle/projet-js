a3e2cd2d408c063bd6ac0a44da555bb1ef1a6661
BayTeo <teo.bay@edu.devinci.fr>
2024-01-10 22:55:55 +0100

    j'essaie de régler les problèmes d'affichage sans succès depuis des heures. Achevez-moi.

--- chifumi/src/App.jsx
+++ chifumi/src/App.jsx
@@ -3,50 +3,59 @@ import { useHistory } from 'react-router-dom';
 import './App.css';
 import database from '../db.json';
 
-export const App = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const history = useHistory();
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
 
-    const handleLogin = (e) => {    
-        e.preventDefault();
+  const handleLogin = (e) => {
+    e.preventDefault();
 
-        const user = database.login.find((user) => user.username === username && user.password === password);
-
-        if (user) {
-            console.log("T'es bien connecté chacal");
-            history.push('/profile'); // Redirect to Profile.jsx
-        } else {
-            if (username !== "Ptdr t ki ?") {
-                setErrorMessage("Ptdr t ki ?");
-            } else if (password !== "Comment ça mon reuf ?") {
-                setErrorMessage("Comment ça mon reuf ?");
-            }
-        }
+    if (username === '' || password === '') {
+      setErrorMessage("Eh oh, tu peux pas laisser des champs vides");
+      return;
     }
 
-    const handleRedirect = () => {
-        history.push('/create-account');
+    const user = database.login.find((user) => user.username === username && user.password === password);
+
+    if (user) {
+      console.log("T'es bien connecté chacal");
+      history.push('/profile');
+    } else {
+      if (username !== "Ptdr t ki ?") {
+        setErrorMessage("Ptdr t ki ?");
+      } else if (password !== "Comment ça mon reuf ?") {
+        setErrorMessage("Comment ça mon reuf ?");
+      }
     }
-    
-    return (
-        <>
-            <form onSubmit={handleLogin}>
-                <div className="form-group">
-                    <label htmlFor="username">Pseudo</label>
-                    <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="Your Username" name="username" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Mot de passe super secret</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} type="Password" placeholder="Password" name="password" />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-            {errorMessage && <p>{errorMessage}</p>}
-            <button onClick={handleRedirect}>Pas de compte ? On te mets bien</button>
-        </>
-    );
+  }
+
+  const handleRedirect = () => {
+    history.push('/create-account');
+  }
+
+  return (
+    <div className="app-container">
+      <form className="login-form" onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="username">Pseudo</label>
+          <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Your Username" name="username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Mot de passe super secret</label>
+          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" name="password" />
+        </div>
+        <div className="button-container">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+      <div className="button-container">
+        <button onClick={handleRedirect}>Pas de compte ? On te mets bien</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
