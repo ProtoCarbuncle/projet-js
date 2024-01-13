f385fd3b07d1f77e57100658f2dfb7abe47e4b50
Matisse Marchand <matisse@mac-de-tody.home>
2024-01-13 22:44:21 +0100

    route

--- chifumi/src/App.jsx
+++ chifumi/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login"
 import CreateAcc from "./Pages/CreateAcc"
 import Profile from "./Pages/Profile"
+import Game from "./Pages/Game";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/CreateAcc" element={<CreateAcc />} />
           <Route path="/Profile" element={<Profile />} />
+          <Route path="/Game" element={<Game/>} />
         </Routes>
       </div>
     </Router>
